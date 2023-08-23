0286c781651a0fa5630d0b2f8143835a054442d1
vyvy-07 <nguyenthuyvy12all@gmail.com>
2023-08-23 21:51:38 +0700

    next

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,30 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  // extends: "react",
+  extends: "airbnb",
   rules: {
-    // "react/no-unescaped-entities": "off",
-    // "@next/next/no-page-custom-font": "off",
-  },
-  parserOptions: {
-    sourceType: "module", // Đảm bảo sourceType là 'module'
+    // JSX
+    "react/prop-types": [
+      1,
+      {
+        ignore: ["className", "children", "location", "params", "location*"],
+      },
+    ],
+    "no-param-reassign": [
+      0,
+      {
+        props: false,
+      },
+    ],
+    "prefer-rest-params": 1,
+    "arrow-body-style": 0,
+    "prefer-template": 0,
+    "react/prefer-stateless-function": 1,
+    "react/jsx-no-bind": [
+      0,
+      {
+        ignoreRefs: false,
+        allowArrowFunctions: false,
+        allowBind: true,
+      },
+    ],
   },
 };
