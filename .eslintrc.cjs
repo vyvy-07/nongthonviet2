ed3be122d681a18f735f40b16a2f2ad0dadc515a
vyvy-07 <nguyenthuyvy12all@gmail.com>
2023-08-23 22:22:53 +0700

    nữa

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,17 @@ module.exports = {
   overrides: [
     {
       files: ["**/*.jsc"],
-
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module", // Đảm bảo đã đặt sourceType là 'module'
+      },
       extends: "next",
       rules: {
         "react/jsx-filename-extension": "off",
         "react/no-unescaped-entities": "off",
         "@next/next/no-page-custom-font": "off",
       },
+      parser: "babel-eslint",
     },
   ],
 };
